a73868e71f3272631442be9556d79cf1e4258e26
Andrea <andrea.zanardo@studenti.unitn.it>
2025-12-03 19:54:07 +0100

    fix json bug

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,30 +1,23 @@
-import express from "express";
-import { logger } from "./middleware/logger.js";
-import routes from "./routes/index.js";
-import { setupSwagger } from "./config/swagger.js";
+const express = require('express');
+const userRoutes = require('./routes/userRoutes');
+
+
+
 
 const app = express();
 
+
 app.use(express.json());
 
-// logging middleware
-app.use(logger);
 
-// swagger docs
-setupSwagger(app);
+app.use("/api/", userRoutes);
 
-// API routes
-app.use("/api/v1", routes);
 
-// Default 404
-app.use((req, res) => {
-  res.status(404).json({ error: "Not found" });
+app.use("/", (req, res) => {
+    res.send("API funzionante");
 });
 
-// Default error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Something broke!" });
-});
 
-export default app;
+
+
+module.exports = app;
