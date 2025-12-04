1209ce559b198a2d272e832ac4e714d7bf6921d3
Mattia <gerardo.mattia20005@gmail.com>
2025-12-04 21:11:18 +0100

    Aggiornamento middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
-
-
-
-
 const app = express();
+
+
+app.use(cors({
+  origin: "http://localhost:3000", // o "*" se vuoi permettere tutti
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true, // se vuoi inviare cookie o auth headers
+}));
 
 
 app.use(express.json());
